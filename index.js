dd49e4a0804355397745885f432c4c1e03d8ddaa
Basirul Billah <basirulbillah@gmail.com>
2022-05-27 22:30:49 +0600

    added jwt

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
@@ -8,9 +9,23 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.r38tq.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'Unauthorize access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'forbidden access' });
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
 
 async function run() {
     try {
@@ -26,6 +41,7 @@ async function run() {
             res.send(products);
         })
 
+        // to get products by id
         app.get('/products/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
@@ -41,7 +57,7 @@ async function run() {
         })
 
         // to load orders
-        app.get('/orders', async (req, res) => {
+        app.get('/orders', verifyJWT, async (req, res) => {
             const query = {};
             const orders = await ordersCollection.find(query).toArray();
             res.send(orders);
@@ -50,19 +66,27 @@ async function run() {
         // to post order 
         app.post('/orders', async (req, res) => {
             const order = req.body;
-            const query = { 
-                product: order.orderName, 
-                quantity: order.orderQuantity, 
+            const query = {
+                product: order.orderName,
+                quantity: order.orderQuantity,
                 customerName: order.customerName,
                 customerAddress: order.customerAddress,
                 customerPhone: order.customerPhone
             };
             const exists = await ordersCollection.findOne(query);
-            if(exists) {
+            if (exists) {
                 return res.send({ success: false, booking: exists });
             }
             const result = await ordersCollection.insertOne(order);
-            return res.send({success: true});
+            return res.send({ success: true });
+        })
+
+        // to delete an order
+        app.delete('/orders/:id', async (req, res) => {
+            const order = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const result = await ordersCollection.deleteOne(filter);
+            res.send(result);
         })
     }
     finally { }
